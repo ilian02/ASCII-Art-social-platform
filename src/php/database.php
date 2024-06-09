9bf0132c765cc411cd0c94cc62d0477047f5235f
ilian02 <ileto02@abv.bg>
2024-06-09 20:53:23 +0300

    added html pages and started working with DB

--- src/php/database.php
+++ src/php/database.php
@@ -1,2 +1,30 @@
 <?php
 
+class Database {
+    private $connection;
+
+
+    public function __construct() {
+        $config = parse_ini_file('../config/config.ini', true);
+
+        $type = $config['db']['db_type'];
+        $host = $config['db']['host'];
+        $name = $config['db']['db_name'];
+        $user = $config['db']['user'];
+        $password = $config['db']['password'];
+
+        $this->init($type, $host, $name, $user, $password);
+    }
+
+    private function init($type, $host, $name, $user, $password) {
+        try {
+            $this->connection = new PDO("$type:host=$host;dbname=$name", $user, $password,
+                array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
+
+            // $this->prepareStatements();
+        } catch(PDOException $e) {
+            echo "Connection failed: " . $e->getMessage();
+        }
+    }
+
+}
