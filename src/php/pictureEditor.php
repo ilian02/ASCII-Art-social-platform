24706aeaf29c1315e87e549cde441f5e050e8c0f
ilian02 <ileto02@abv.bg>
2024-06-21 18:07:46 +0300

    Added stuff

--- src/php/pictureEditor.php
+++ src/php/pictureEditor.php
@@ -4,7 +4,13 @@
     
     if ($_SERVER['REQUEST_METHOD'] === 'GET') {
         
-        echo json_encode(array('status' => 'success', 'message' => 'nice'));
+        $data = $_GET['param'];
+
+        if ($parametres['pic_name']) {
+            echo json_encode(array('status' => 'success', 'message' => 'bruh'));
+        } else {
+            echo json_encode(array('status' => 'success', 'message' => 'nice'));
+        }
     }
     
 ?>
